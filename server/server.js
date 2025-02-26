c0182af54eb39fc38c3910a1706ac5280c5bc09f
NIKHIL ADAPUREDDY <166306404+nikkhil9@users.noreply.github.com>
2025-02-26 16:58:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://authenticate12.netlify.app/']
 
 app.use(express.json());
 app.use(cookieParser());
